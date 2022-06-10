31a782ebc1fe4c6853b4d8100ad6500a07b4e084
mzmz02 <bb.mai.k0702@gmail.com>
2022-06-10 22:23:15 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/show" => "posts#show"
+
+  post "posts/yes" => "posts#yes"
+  post "posts/no" => "posts#no"
+
+  post "posts/stop" => "posts#stop"
+  post "posts/start" => "posts#start"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
